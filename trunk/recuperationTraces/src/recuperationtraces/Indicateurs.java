5d75a49711ddd0a5c878633df47ef58fee99cc80
said.rjimati@gmail.com <said.rjimati@gmail.com@9d9e2083-61d5-5a72-09b0-61e803df20c9>
2014-06-02 11:45:42 +0000

    teste

---
+++ trunk/recuperationTraces/src/recuperationtraces/Indicateurs.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package recuperationtraces;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+/**
+ * spécification de la structure de données qui contiendra les traces
+ * @author Matthieu et Said
+ * la classe qui implementera cette interface héritera de HashMap
+ * clé : nom de l'indicateur
+ * valeur : une liste des valeurs de cet indicateur
+ */
+
+public interface Indicateurs {
+    
+    /**
+     * Crée une HashMap d'indicateurs qui contiendra une partie de la HashMap initiale
+     * permet de regrouper dans une "sous-HashMap" une liste d'indicateurs
+     * @param ARechercher la liste des indicateurs à mettre dans la vue
+     * @return HashMap contenant la vue
+     * @exemple les SAE
+     */
+    public Indicateurs creerVueIndicateurs ( List<String> ARechercher);
+    
+    /**
+     * Crée une HashMap d'indicateurs qui contiendra une partie de la HashMap initiale
+     * permet de regrouper dans une "sous-HashMap" les indicateurs qui vérifient la condition
+     * @param nomIndicateur l'indicateur concerné par la contrainte
+     * @param ValeurContrainte la valeur qui définit la contrainte
+     * @param condition la condition de la contrainte : =, >, <, >=, <=
+     * @return HashMap contenant la vue
+     * @exemple les participants qui ont lu l'introduction en moins de 10 secondes
+     * @exemple les participants qui ont eu l'aide Scaffolding
+     */
+    public Indicateurs creerVueValeurs (String nomIndicateur, String ValeurContrainte, String condition);
+    
+    
+    /**
+     * Crée une matrice à partir des données de la HashMap
+     * qui sera utilisable par R
+     * @return la matrice
+     */
+    public double[][] creerMatrice();
+    
+    /**
+     * Crée une table à partir des données de la HashMap
+     * qui sera utilisable par R 
+     * Dans le cas, où la HashMap ne contient q'une ligne
+     * @return la table
+     */
+    public double[] creerTable();
+    
+    /**
+     * Crée une fenetre qui affiche la HashMap
+     * permettra de consulter les données contenues dans la HashMap
+     */
+    public void afficher();
+    
+    /**
+     * supprime le participant
+     * @param indice l'indice du participant à supprimer (ou nom du participant)
+     */
+    public void supprimerParticipants(int indice);
+    
+    public void setTr(HashMap<String, List<String>> tr);
+    public HashMap<String, List<String>> getTr();
+    /**
+     * pour ajouter un indicateur nous utiliserons la méthode put de la classe Hashmap
+     * pour supprimer un indicateur nous utiliserons la méthode remove de la classe Hashmap
+     * on pourra créer des copies d'une HashMap grace à la methode clone
+     */
+    
+    
+}
+
---
+++ trunk/recuperationTraces/src/recuperationtraces/Racine.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package recuperationtraces;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * classe racine de l'application qui contiendra une HashMap 
+ * contenant uniquement les données remontées du serveur
+ * @author Tigro
+ */
+public class Racine {
+    //ensuite, on créera une liste d'IndicateurImpl qui contiendra toutes les vues 
+    //IndicateurImpl qui contiendra les données remonté du serveur
+    //on ne le modifiera pas on créera des copies pour les modifications
+    IndicateursImpl traces; 
+    IndicateursImpl tracesEtIndicateursCalcules;
+    /**
+     * constructeur de la racine
+     */
+    public Racine() {
+        File fichier = new File("src/sauvegardeIndicateurs.dat");
+        if (fichier.exists()){
+            System.out.println("on charge le fichier");
+            traces = new IndicateursImpl(this);
+            traces.charger(fichier);
+        }else {
+            System.out.println("on recupere les traces");
+            recupererTrace();
+            System.out.println("fin de recupererTrace : on va sauvegarder");
+            traces.sauver(fichier);
+            System.out.println("fin de la sauvegarde");
+        }
+        tracesEtIndicateursCalcules = new IndicateursImpl(this);
+        tracesEtIndicateursCalcules.setTr(traces.getTr());
+        
+        //on construit la Hashmap qui contiendra les indicateurs remontés du serveur 
+        //et les indicateurs calculés
+        tracesEtIndicateursCalcules.calculerMoyenneNoteNormalisee();
+        
+        tracesEtIndicateursCalcules.afficher();
+    }
+    
+   /**
+    * une méthode pour recuprer les Traces du serveur et les sauvegarder dans une HashMap 
+    * pour pouvoir l'utiliser aprés sans avoir à se reconnecter à chaque fois 
+    * 
+    */
+    private void recupererTrace(){
+        RecupTrace dialog = new RecupTrace(new javax.swing.JFrame(), true,this);
+        System.out.println("fin de recupTrace");
+        traces=dialog.getTraces();
+        
+    }
+
+    public IndicateursImpl getTraces() {
+        return traces;
+    }
+    
+}
+    
+  
+    /**
+     * La classe IndicateurImpl prend la classe racine en parametre pour pouvoir utiliser
+     * la HashMap qui contient les Traces Sauvgardées
+     */
+
