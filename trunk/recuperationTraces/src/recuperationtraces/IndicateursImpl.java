4588985006c3f4e1559045e2a453094011885cac
said.rjimati@gmail.com <said.rjimati@gmail.com@9d9e2083-61d5-5a72-09b0-61e803df20c9>
2014-06-05 15:43:43 +0000

    ajout du packages IHM (un premier ecrant juste pour tester)

--- trunk/recuperationTraces/src/recuperationtraces/IndicateursImpl.java
+++ trunk/recuperationTraces/src/recuperationtraces/IndicateursImpl.java
@@ -26,10 +26,12 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
 
 //test SVN !!!! n°3!!
 
-public class IndicateursImpl implements Indicateurs {
+public class IndicateursImpl implements Indicateurs, TableModel {
     
     private HashMap<String, List<String>> tr;
     
@@ -620,6 +622,51 @@ public class IndicateursImpl implements Indicateurs {
         listeComportementRA.add("Moyenne_BcC");
         creerIndicateurMoyenne("Moyenne_ComportementRA",listeComportementRA );
         
+    }
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
     
