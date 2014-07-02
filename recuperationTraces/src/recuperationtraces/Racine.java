d4e5155077a5097d49889fa8a0136757b3eca008
said.rjimati@gmail.com <said.rjimati@gmail.com@9d9e2083-61d5-5a72-09b0-61e803df20c9>
2014-07-02 16:06:46 +0000

    ajout de sauver et charger dans racine (trace/traceindicateurcalculer/groupe) / suppression de sauver et charger de IndicateurImpl modification de la méthode Object[][] transofrmerEnTableObject()

--- recuperationTraces/src/recuperationtraces/Racine.java
+++ recuperationTraces/src/recuperationtraces/Racine.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,7 +31,7 @@ import rjava.acces.ClientR;
  * contenant uniquement les données remontées du serveur
  * @author Tigro
  */
-public class Racine {
+public class Racine implements Serializable{
     //ensuite, on créera une liste d'IndicateurImpl qui contiendra toutes les vues 
     //IndicateurImpl qui contiendra les données remonté du serveur
     //on ne le modifiera pas on créera des copies pour les modifications
